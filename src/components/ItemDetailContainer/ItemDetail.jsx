70398cb0cdfa2979dbb012e20c3c18d60fd362a3
Nicolás Vera <nicoverit4@gmail.com>
2024-07-30 15:19:25 -0300

    Añadido ItemCount y estilos del mismo

--- src/components/ItemDetailContainer/ItemDetail.jsx
+++ src/components/ItemDetailContainer/ItemDetail.jsx
@@ -1,4 +1,12 @@
+import ItemCount from "../ItemCount/ItemCount";
+
 const ItemDetail = ({ producto }) => {
+  const agregarAlCarrito = (contador) => {
+    const productoCarrito = { ...producto, cantidad: contador };
+
+    console.log(productoCarrito);
+  };
+
   return (
     <div className="item-detail">
       <div className="image-detail">
@@ -8,6 +16,7 @@ const ItemDetail = ({ producto }) => {
         <p className="name-detail">{producto.nombre}</p>
         <p className="text-detail">Descripción: {producto.descripcion}</p>
         <p className="text-detail">Precio: ${producto.precio}</p>
+        <ItemCount stock={producto.stock} agregarAlCarrito={agregarAlCarrito} />
       </div>
     </div>
   );
